e16590e161c370addf3dbc02b5c256b01c81aea0
samk1r0 <artalt3@gmail.com>
2022-05-23 10:23:48 +0200

    gsap animation added

--- assets/src/main.js
+++ assets/src/main.js
@@ -6,6 +6,7 @@ import Headroom from "headroom.js";
 import Swup from "swup";
 import SwupScrollPlugin from "@swup/scroll-plugin";
 import SwupBodyClassPlugin from "@swup/body-class-plugin";
+import { gsap } from "gsap";
 
 import { initPopups, closePopupInit } from "./init-popups";
 import { initAccordeon } from "./init-accordeon";
@@ -63,6 +64,12 @@ domReady(function () {
   document.querySelector(".core_loader").classList.remove("-show");
   setTimeout(function () {
     document.querySelector(".core_loader").classList.remove("-front");
+    
+    document.querySelectorAll('.hero-illustration-title').forEach((el,i)=>{
+      let animation = gsap.fromTo(el, {opacity: 0, translateY:-15}, {opacity: 1, translateY:0, duration: 1, ease: "Power2.easeOut" });
+      animation.delay(i*0.1)
+    })
+    
     initSwup();
     initAll();
   }, delayTimer);
